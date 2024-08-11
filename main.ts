404f428503f2409f5e360370f1446fa6948764bc
omeruysal81 <157880761+omeruysal81@users.noreply.github.com>
2024-08-11 18:03:59 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.showString("I")
+basic.showIcon(IconNames.Heart)
+basic.showString("Blok Kodlama")
+basic.forever(function () {
+    basic.showString("I")
+    basic.showIcon(IconNames.Heart)
+    basic.showString("Blok Kodlama")
+})
